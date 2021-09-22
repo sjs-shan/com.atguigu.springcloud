d16488afbdfb7c90e2988934a996b04bf0c243ee
sjs <sjs12345678>
2021-09-23 01:28:53 +0800

    第四次提交

---
+++ gmall-passport-web/src/test/java/com/atguigu/gmall0218/passport/GmallPassportWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.gmall0218.passport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallPassportWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
